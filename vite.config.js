52302293eecbbe1ad45def5496feda9f233eb9e6
Naveen kumar <126279118+iamnaveen8851@users.noreply.github.com>
2025-04-08 13:54:01 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'ad26-2405-201-4021-1a06-4f9-9612-eb82-628a.ngrok-free.app',
+    ],
+  },
 })
